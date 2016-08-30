1d3cf078584022d1e94e18e917796f5497858c95
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2016-08-30 18:39:36 +0200

    client: minor lambdas in PreviewManager

--- client/app/src/main/java/org/faudroids/mrhyde/jekyll/PreviewManager.java
+++ client/app/src/main/java/org/faudroids/mrhyde/jekyll/PreviewManager.java
@@ -6,7 +6,6 @@ import org.faudroids.mrhyde.git.FileManager;
 import javax.inject.Inject;
 
 import rx.Observable;
-import rx.functions.Func1;
 import timber.log.Timber;
 
 public class PreviewManager {
@@ -27,20 +26,12 @@ public class PreviewManager {
 	public Observable<String> loadPreview(FileManager fileManager) {
 		Timber.d("starting new preview");
 		return repoDetailsFactory.createRepoDetails(fileManager )
-				.flatMap(new Func1<RepoDetails, Observable<PreviewResult>>() {
-					@Override
-					public Observable<PreviewResult> call(RepoDetails repoDetails) {
-						Timber.d("found " + repoDetails.getStaticFiles().size() + " binary files for preview");
-						for (BinaryFile file : repoDetails.getStaticFiles()) Timber.d(file.getPath());
-						return jekyllApi.createPreview(repoDetails);
-					}
-				})
-				.flatMap(new Func1<PreviewResult, Observable<String>>() {
-					@Override
-					public Observable<String> call(PreviewResult previewResult) {
-						return Observable.just(previewResult.getPreviewUrl());
-					}
-				});
+				.flatMap(repoDetails -> {
+          Timber.d("found " + repoDetails.getStaticFiles().size() + " binary files for preview");
+          for (BinaryFile file : repoDetails.getStaticFiles()) Timber.d(file.getPath());
+          return jekyllApi.createPreview(repoDetails);
+        })
+				.flatMap(previewResult -> Observable.just(previewResult.getPreviewUrl()));
 
 	}
 
