7cb7f67551b13f9d6e6f6ea33de9f9182de64c31
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2016-10-08 21:35:34 +0200

    client: setup GA

--- client/app/src/main/java/org/faudroids/mrhyde/app/MrHydeApp.java
+++ client/app/src/main/java/org/faudroids/mrhyde/app/MrHydeApp.java
@@ -3,9 +3,14 @@ package org.faudroids.mrhyde.app;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.StandardExceptionParser;
+import com.google.android.gms.analytics.Tracker;
 import com.karumi.dexter.Dexter;
 
 import org.faudroids.mrhyde.BuildConfig;
+import org.faudroids.mrhyde.R;
 import org.faudroids.mrhyde.bitbucket.BitbucketModule;
 import org.faudroids.mrhyde.git.GitModule;
 import org.faudroids.mrhyde.github.GitHubModule;
@@ -16,6 +21,7 @@ import timber.log.Timber;
 public class MrHydeApp extends Application {
 
   private AppComponent component;
+  private Tracker analyticsTracker;
 
   @Override
   public void onCreate() {
@@ -30,13 +36,18 @@ public class MrHydeApp extends Application {
         .bitbucketModule(new BitbucketModule())
         .build();
 
+    // setup google analytics
+    GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+    analyticsTracker = analytics.newTracker(R.xml.global_tracker);
+    analyticsTracker.enableAutoActivityTracking(true);
+    analyticsTracker.enableExceptionReporting(true);
+
     // setup logging
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
+      analytics.setAppOptOut(true);
     } else {
-      Timber.plant(new Timber.DebugTree());
-      // Fabric.with(this, new Crashlytics());
-      // Timber.plant(new CrashReportingTree());
+      Timber.plant(new CrashReportingTree());
     }
 
     // setup permission requests
@@ -47,32 +58,44 @@ public class MrHydeApp extends Application {
     return component;
   }
 
+  public Tracker getAnalyticsTracker() {
+    return analyticsTracker;
+  }
 
-  /*
-  private static final class CrashReportingTree extends Timber.HollowTree {
+  private final class CrashReportingTree extends Timber.HollowTree {
 
-		@Override
-		public void e(String msg, Object... args) {
-			Crashlytics.log(msg);
-		}
+    private final Timber.DebugTree debugTree = new Timber.DebugTree();
 
-		@Override
+    @Override
+    public void e(String message, Object... args) {
+      debugTree.e(message, args);
+    }
+
+    @Override
 		public void e(Throwable e, String msg, Object... args) {
-			// Crashlytics.log(msg);
-			// Crashlytics.logException(e);
+      debugTree.e(e, msg, args);
+      reportThrowable(e);
 		}
 
-		@Override
-		public void w(String msg, Object... args) {
-			Crashlytics.log(msg);
-		}
+    @Override
+    public void w(String message, Object... args) {
+      debugTree.w(message);
+    }
 
 		@Override
 		public void w(Throwable e, String msg, Object... args) {
-			// Crashlytics.log(msg);
-			// Crashlytics.logException(e);
+      debugTree.w(e, msg, args);
+      reportThrowable(e);
 		}
 
-	}
-	*/
+    private void reportThrowable(Throwable e) {
+      analyticsTracker.send(new HitBuilders.ExceptionBuilder()
+          .setDescription(new StandardExceptionParser(MrHydeApp.this, null)
+              .getDescription(Thread.currentThread().getName(), e))
+          .setFatal(false)
+          .build()
+      );
+    }
+
+  }
 }
--- client/app/src/main/java/org/faudroids/mrhyde/ui/utils/AbstractActivity.java
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/utils/AbstractActivity.java
@@ -7,7 +7,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.analytics.Tracker;
+
 import org.faudroids.mrhyde.R;
+import org.faudroids.mrhyde.app.MrHydeApp;
 
 import javax.inject.Inject;
 
@@ -17,58 +20,56 @@ import rx.subscriptions.CompositeSubscription;
 
 public abstract class AbstractActivity extends AppCompatActivity {
 
-	public final CompositeSubscription compositeSubscription = new CompositeSubscription();
-	@Inject protected UiUtils uiUtils;
-	@BindView(R.id.spinner) protected View spinnerContainerView;
-	@BindView(R.id.spinner_image) protected ImageView spinnerImageView;
+  public final CompositeSubscription compositeSubscription = new CompositeSubscription();
+  @Inject protected UiUtils uiUtils;
+  @BindView(R.id.spinner) protected View spinnerContainerView;
+  @BindView(R.id.spinner_image) protected ImageView spinnerImageView;
+  protected Tracker analyticsTracker;
 
   protected boolean showBackButton = true;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		// show action bar back button
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null && showBackButton) {
-			actionBar.setDisplayHomeAsUpEnabled(true);
-			actionBar.setHomeButtonEnabled(true);
-		}
-	}
+    // show action bar back button
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null && showBackButton) {
+      actionBar.setDisplayHomeAsUpEnabled(true);
+      actionBar.setHomeButtonEnabled(true);
+    }
 
+    analyticsTracker = ((MrHydeApp) getApplication()).getAnalyticsTracker();
+  }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// handle action bar back buttons
-		switch(item.getItemId()) {
-			case android.R.id.home:
-				onBackPressed();
-				return true;
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // handle action bar back buttons
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        onBackPressed();
+        return true;
 
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    }
+    return super.onOptionsItemSelected(item);
+  }
 
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    compositeSubscription.unsubscribe();
+  }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		compositeSubscription.unsubscribe();
-	}
+  public void showSpinner() {
+    uiUtils.showSpinner(spinnerContainerView, spinnerImageView);
+  }
 
+  public void hideSpinner() {
+    uiUtils.hideSpinner(spinnerContainerView, spinnerImageView);
+  }
 
-	public void showSpinner() {
-		uiUtils.showSpinner(spinnerContainerView, spinnerImageView);
-	}
-
-
-	public void hideSpinner() {
-		uiUtils.hideSpinner(spinnerContainerView, spinnerImageView);
-	}
-
-
-	public boolean isSpinnerVisible() {
-		return uiUtils.isSpinnerVisible(spinnerContainerView);
-	}
+  public boolean isSpinnerVisible() {
+    return uiUtils.isSpinnerVisible(spinnerContainerView);
+  }
 
 }
